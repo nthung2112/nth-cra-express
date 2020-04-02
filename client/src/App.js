37ea6a0cfa6124310ca7374b898b2226d64cac3c
Hung Nguyen <nthung2112@gmail.com>
2020-04-02 15:09:26 +0700

    Update all package version

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
-  const [response, setResponse] = useState('');
-  const [post, setPost] = useState('');
-  const [responseToPost, setResponseToPost] = useState('');
-
-  useEffect(() => {
-    const callApi = async () => {
-      const response = await fetch('/api/hello');
-      const body = await response.json();
-      if (response.status !== 200) throw Error(body.message);
-      setResponse(body.express);
-    };
-    callApi();
-  }, []);
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ post })
-    });
-    const body = await response.text();
-
-    setResponseToPost(body);
-  };
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -47,15 +19,8 @@ export default function App() {
           Learn React
         </a>
       </header>
-      <p>{response}</p>
-      <form onSubmit={handleSubmit}>
-        <p>
-          <strong>Post to Server:</strong>
-        </p>
-        <input type="text" value={post} onChange={e => setPost(e.target.value)} />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{responseToPost}</p>
     </div>
   );
 }
+
+export default App;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const bodyParser = require('body-parser');
+
+// need to add in case of self-signed certificate connection
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+// Import config file
+require('dotenv');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,19 +27,13 @@ app.disable('etag');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
-});
-
 if (process.env.NODE_ENV === 'production') {
+  // Add compression
+  app.use(compression());
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build'), {
+    maxAge: '1y', // One week
+  }));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
